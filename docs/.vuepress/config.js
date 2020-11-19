645d5c3ccd24d0fc1afbd298a6b68471350f3bca
heshiyu <450979309@qq.com>
2020-11-19 09:20:41 +0800

    feat: react-ssr

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -161,6 +161,7 @@ const sidebar = {
                 'react/fiber/',
                 'react/life-cycle/',
                 'react/set-state/',
+                'react/react-ssr/',
                 'react/react-composition/',
                 'react/hooks/',
                 'react/redux/',
