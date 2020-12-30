f59c26811a4e87e2af92598b1f14aa4eeba1ab54
heshiyu <450979309@qq.com>
2020-12-30 10:21:50 +0800

    feat: CI配置

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-##set -e
-# npm install -g vuepress@next
+set -e
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
-echo '# Welcome! And You can enter my blog by： [https://Heshiyu1996.github.io]('https://Heshiyu1996.github.io')' > README.md
 
 git init
 git add -A
@@ -20,9 +15,11 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f https://${token}@${address} master:master
-git push -f git@github.com:Heshiyu1996/Heshiyu1996.github.io.git master
+# git push -f https://github.com/Heshiyu1996/Heshiyu1996.github.io.git master
+# git push -f git@github.com:Heshiyu1996/Heshiyu1996.github.io.git master
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/Heshiyu1996/blog.git master:gh-pages
 
 cd -
